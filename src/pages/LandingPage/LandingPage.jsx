480ca7a3fa9480bb0049c824cb50641c52c8603f
YBRpcBrian <yabiunbrian1@gmail.com>
2025-05-09 14:20:28 +0100

    landing page in progress

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './Hero'
 import BannerFeatures from './BannerFeature'
+import ConverterPage from './ConvertPage'
 
 const LandingPage = () => {
   return (
     <>
     <Hero />
     <BannerFeatures />
+    <ConverterPage />
     
     </>
   )
