8844b85a4ef57fc8dd92dc55c4f6fbf693760790
YBRpcBrian <yabiunbrian1@gmail.com>
2025-05-10 08:45:08 +0100

    payment success

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Api = axios.create({
-  baseURL: 'http://localhost:5000', // replace with your real backend API URL
+  baseURL: 'https://tchokopay-backend-4.onrender.com', // replace with your real backend API URL
   headers: {
     'Content-Type': 'application/json',
   },
